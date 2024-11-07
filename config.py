047dc1b968be986bb88a9292ee82d3a4868e3416
soumit1803063 <somudasctgbn@gmail.com>
2024-11-07 21:25:24 +0600

    Add streamlit secret management

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 # Load environment variables
-load_dotenv()
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+# load_dotenv()
+GROQ_API_KEY = st.secrets['GROQ_API_KEY']#os.getenv('GROQ_API_KEY')
