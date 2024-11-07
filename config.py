2fd620bc1ec034102b94a0df9265dadf07480a36
soumit1803063 <somudasctgbn@gmail.com>
2024-11-07 21:34:58 +0600

    Remove code related to load from .env

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 # config.py
 import os
-from dotenv import load_dotenv
 import streamlit as st
-# Load environment variables
-# load_dotenv()
-GROQ_API_KEY = st.secrets['GROQ_API_KEY']#os.getenv('GROQ_API_KEY')
+
+GROQ_API_KEY = st.secrets['GROQ_API_KEY']
